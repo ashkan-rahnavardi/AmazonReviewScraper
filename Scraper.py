5eeb4c5fafe8d6ac23f05d0a36c95c6aa839473d
ashkan-rahnavardi <ashkan.rahnavardi@gmail.com>
2023-06-10 13:21:35 -0700

    automatically gets all product reviews from a given amazon product URL

--- Scraper.py
+++ Scraper.py
@@ -1,17 +1,30 @@
 from requests_html import HTMLSession
+import re
 
 
 class Reviews:
-    def __init__(self, asin) -> None:
-        self.asin = asin
+    def __init__(self, product_url) -> None:
+        self.product_url = product_url
         self.session = HTMLSession()
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15'}
-        self.url = f'https://www.amazon.ca/Floating-OMOUBOI-Swimming-Snorkeling-Inflatable/product-reviews/{self.asin}/ref=cm_cr_arp_d_paging_btm_next_2?ie=UTF8&reviewerType=all_reviews&pageNumber='
+
+    def get_asin(self):
+        """Returns the asin for the given amazon product URL"""
+        asins = re.findall(r'/[dg]p/([^/]+)', self.product_url, flags=re.IGNORECASE)
+        return asins[0]
 
     def pagination(self, page):
-        r = self.session.get(self.url + str(page))
-        r.html.render(sleep=1)
+        """
+        Returns the html for the product review page.
+
+        :param page: the page number of the review page.
+        :return: html if product reviews exist on given page, False if no reviews exist.
+        """
+        asin = self.get_asin()
+        review_url = f'https://amazon.ca/product-reviews/{asin}/?pageNumber='  # URL for product review page
+        r = self.session.get(review_url + str(page))
+        r.html.render()
         if not r.html.find('div[data-hook=review]'):
             return False
         else:
@@ -34,6 +47,17 @@ class Reviews:
 
 
 if __name__ == '__main__':
-    amz = Reviews('B08K32JFR7')
-    reviews = amz.pagination(1)
-    print(amz.parse(reviews))
+    amz = Reviews('https://www.amazon.ca/Floating-OMOUBOI-Swimming-Snorkeling-Inflatable/dp/B08K32JFR7/ref=cm_cr_arp_d_product_top?ie=UTF8')
+    all_reviews = []
+    reviews_exist = True
+    page_number = 1
+    while reviews_exist:
+        print('getting page ', page_number)
+        reviews = amz.pagination(page_number)
+        if reviews is not False:
+            all_reviews.append(amz.parse(reviews))
+            page_number += 1
+        else:
+            reviews_exist = False
+            print('no more pages')
+    print(all_reviews)
