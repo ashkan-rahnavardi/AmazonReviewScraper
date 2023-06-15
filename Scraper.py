f18e7feba0ab82c54e52be10e6c456c96fda9c96
ashkan-rahnavardi <ashkan.rahnavardi@gmail.com>
2023-06-14 19:22:44 -0700

    Made a function to get all the reviews

--- Scraper.py
+++ Scraper.py
@@ -8,6 +8,7 @@ class Reviews:
         self.session = HTMLSession()
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.4 Safari/605.1.15'}
+        self.reviews = []
 
     def get_asin(self):
         """Returns the asin for the given amazon product URL"""
@@ -31,7 +32,11 @@ class Reviews:
             return r.html.find('div[data-hook=review]')
 
     def parse(self, reviews):
-        total = []
+        """
+        Parses raw html reviews into a readable format and adds it to self.reviews.
+
+        :param reviews: html review
+        """
         for review in reviews:
             title = review.find('a[data-hook=review-title]', first=True).text
             rating = review.find('i[data-hook=review-star-rating] span', first=True).text
@@ -42,22 +47,25 @@ class Reviews:
                 'rating': rating,
                 'body': body
             }
-            total.append(data)
-        return total
+            self.reviews.append(data)
+
+    def get_all_reviews(self):
+        """Loops through review pages until no reviews are found."""
+        reviews_exist = True
+        page_number = 1
+        while reviews_exist:
+            print('getting page ', page_number)
+            reviews = self.pagination(page_number)
+            if reviews is not False:
+                self.parse(reviews)
+                page_number += 1
+            else:
+                reviews_exist = False
+                print('no more pages')
 
 
 if __name__ == '__main__':
     amz = Reviews('https://www.amazon.ca/Floating-OMOUBOI-Swimming-Snorkeling-Inflatable/dp/B08K32JFR7/ref=cm_cr_arp_d_product_top?ie=UTF8')
-    all_reviews = []
-    reviews_exist = True
-    page_number = 1
-    while reviews_exist:
-        print('getting page ', page_number)
-        reviews = amz.pagination(page_number)
-        if reviews is not False:
-            all_reviews.append(amz.parse(reviews))
-            page_number += 1
-        else:
-            reviews_exist = False
-            print('no more pages')
-    print(all_reviews)
+    amz.get_all_reviews()
+    print(amz.reviews)
+
